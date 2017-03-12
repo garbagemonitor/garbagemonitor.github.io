979bebab4b88f2a760e2aa3910f5caa0ed4700c2
garbagemonitor <garbagemonitor@gmail.com>
2017-03-12 12:19:53 +0530

    3rd change

---
+++ Template.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css" />
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="banner">             
+            </div>
+            
+            <nav id="navigation">
+                <ul id="nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    
+                </ul>
+            </nav>
+            <div id="content_area">
+				<a href="dustbin1.php"><button type="button" style="color:red;border-radius:50px;height:50px;width:80px;font-size:15px;position: relative; top:5px; left: 10px;">DustBin1</button></a>
+				<a href="dustbin2.php"><button type="button" style="color:red;border-radius:50px;height:50px;width:80px;font-size:15px;position: relative; top:150px; left: 135px;">DustBin2</button></a>
+				<a href="dustbin3.php"><button type="button" style="color:red;border-radius:50px;height:50px;width:80px;font-size:15px;position: relative; top:325px; left: 120px;">DustBin3</button></a>
+            </div>
+            
+           
+            
+            <footer>
+                <p>All rights reserved</p>
+            </footer>
+        </div>
+    </body>
+</html>
